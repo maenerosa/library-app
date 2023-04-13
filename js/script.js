8ad16663feef69a262ac2b18682a7133766bd69e
kodego-mae <funj451@gmail.com>
2023-04-13 19:56:40 +0700

    update files

--- js/script.js
+++ js/script.js
@@ -1,11 +1,42 @@
-const openModal = document.getElementById("open-modal");
-const closeModal = document.getElementById("cancel-modal");
+const openModalBtn = document.getElementById("open-modal");
+const closeModalBtn = document.getElementById("close-modal");
 const modal = document.getElementById("modal");
+const addBtn = document.getElementById("add-book-btn");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const library = [];
 
-openModal.addEventListener("click", function () {
-  modal.style.display = "flex";
-});
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
 
-closeModal.addEventListener("click", function () {
+function clearInputs() {
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+}
+
+function closeModal() {
   modal.style.display = "none";
+}
+
+function openModal() {
+  modal.style.display = "flex";
+}
+
+addBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  const book = new Book(title.value, author.value, pages.value);
+
+  library.push(book);
+
+  clearInputs();
+  closeModal();
 });
+
+openModalBtn.addEventListener("click", openModal);
+
+closeModalBtn.addEventListener("click", closeModal);
